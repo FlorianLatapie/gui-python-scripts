c4b27f5b703290d2789fa9b0b912ae2352b5c97a
Florian Latapie <florian.latapie@etu.unice.fr>
2023-08-06 21:17:31 +0700

    improvements on the readability of the code and stateless now

--- comparer.py
+++ comparer.py
@@ -1,24 +1,11 @@
 import csv
 
-# Defining variables
-line = 0
-errors = []
-
-
-def error_text():
+def format_error_messages(errors):
     if not errors:
         return "aucune erreur"
-    text = ""
-    for error in errors:
-        text += f"{error[0]}\nFichier 1 : {error[1]}\nFichier 2 : {error[2]}\n\n"
-    return text
-
+    return "\n".join(f"Ligne {error[0]} :\n{error[1]}\n{error[2]}\n" for error in errors)
 
 def run(file_name_1, file_name_2, row_to_search_1=0, row_to_search_2=0, entire_line=False):
-    global line
-    line = 0
-
-    global errors
     errors = []
 
     # Open both files at the same time
@@ -26,26 +13,27 @@ def run(file_name_1, file_name_2, row_to_search_1=0, row_to_search_2=0, entire_l
         reader1 = csv.reader(f1, delimiter=';')
         reader2 = csv.reader(f2, delimiter=';')
 
-        # Iterate on the shortest file to stop at the shortest file
-        for row in reader1:
-            line += 1
-            row1 = row
-            row2 = reader2.__next__()
-            # Iterate on the second file at the same time as the first
+        # iterate over the two files at the same time using zip
+        for (i, row) in enumerate(zip(reader1, reader2)):
+            line = i + 1
+            f1_row = row[0]
+            f2_row = row[1]
 
             if entire_line:
-                val1 = " ".join(row1)
-                val2 = " ".join(row2)
-
-                if val1 != val2:
-                    errors.append([line, row1, row2])
+                val1 = "[" + ", ".join(f1_row) + "]"
+                val2 = "[" + ", ".join(f2_row) + "]"
             else:
                 # Delete useless characters
-                val1 = row1[row_to_search_1].replace("/", " ").replace("	", " ").replace("  ", " ")
-                val2 = row2[row_to_search_2].replace("/", " ").replace("	", " ").replace("  ", " ")
-                if val1 != val2:
-                    errors.append([line, val1, val2])
+                val1 = f1_row[row_to_search_1].replace("/", " ").replace("	", " ").replace("  ", " ")
+                val2 = f2_row[row_to_search_2].replace("/", " ").replace("	", " ").replace("  ", " ")
 
+            if val1 != val2:
+                errors.append([line, val1, val2])
+
+        return errors
+
+
+def export_errors(errors, file_name):
     # Save the result in a .csv file
     # newline='' because "writerow" adds a newline by default
     with open('res.csv', 'w', newline='') as csvfile:
@@ -53,7 +41,7 @@ def run(file_name_1, file_name_2, row_to_search_1=0, row_to_search_2=0, entire_l
         for row in errors:
             csv_writer.writerow(row)
 
-
 if __name__ == "__main__":
-    run("Sample files/compare1.csv", "Sample files/compare2.csv", entire_line=True)
-    print(error_text())
+    errors = run("Sample files/compare1.csv", "Sample files/compare2.csv", entire_line=True)
+    print(format_error_messages(errors))
+    #export_errors(errors, "res.csv")
--- compter.py
+++ compter.py
@@ -1,17 +1,27 @@
 import csv
 
 
+def convert_row_to_search(row):
+    if not row.isnumeric() and row.isalpha():
+        row = row.upper()
+        col = 0
+        for c in row:
+            col = col * 26 + (ord(c) - 64)
+        row = col
+    return int(row) - 1
+
+
 def format_error_messages(errors):
     if not errors:
         return "aucune erreur"
-    return '\n'.join(f"Ligne {error[0]} : {error[1]}" for error in errors)
+    return '\n'.join(f"Ligne {error[0]} ({error[1]}): {error[2]}" for error in errors)
 
 
-def run(file_name, row_to_search, has_header=False):
+def run(file_name, row_number_to_search, has_header=False):
     errors = []
 
-    def add_error(line_number, content):
-        errors.append([line_number, content])
+    def add_error(line_number, reason, content):
+        errors.append((line_number, reason, content))
 
     with open(file_name, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -19,19 +29,18 @@ def run(file_name, row_to_search, has_header=False):
         if has_header:
             next(reader)
 
-        last_num = next(reader)[row_to_search]
+        last_num = next(reader)[row_number_to_search]
 
         for (i, row) in enumerate(reader):
             current_line_number = i + 2
+            if not row[row_number_to_search].isnumeric():
+                add_error(current_line_number, "Erreur de lecture", row)
+            elif int(row[row_number_to_search]) != int(last_num) + 1:
+                add_error(current_line_number, "Pas consécutif", row)
 
-            if not isinstance(int(row[row_to_search]), int):
-                add_error(current_line_number, row)
-            if int(row[row_to_search]) != int(last_num) + 1:
-                add_error(current_line_number, row)
-
-            last_num = row[row_to_search]
+            last_num = row[row_number_to_search]
     return errors
 
 
 if __name__ == "__main__":
-    print(format_error_messages(run("Sample files/count - with error.csv", 1)))
+    print(format_error_messages(run("Sample files/count - with error.csv", convert_row_to_search("2"))))
